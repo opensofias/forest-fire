65b969b2b5c317962320c11fd484df7ce7c9c75e
sofia <12581975+opensofias@users.noreply.github.com>
2023-09-09 07:02:53 +0200

    uploading the thing

---
+++ main.js
@@ -0,0 +1,40 @@
+const calculate = depth => {
+	const result = new Int32Array (depth)
+	let progress = 0
+	while (progress < depth) {
+		const avoid = new Set ()
+		let steps = 1
+		while (steps * 2 < progress + 1) {
+			avoid.add (
+				2 * result [progress - steps] -
+				result [progress - steps * 2]
+			)
+			steps ++
+		}
+		let candidate = 0
+		while (avoid.has (candidate)) candidate ++
+		result [progress] = candidate
+		progress ++
+	}
+	return result
+}
+
+const scatterplot = (sequence, zoomOut = 1, opacity = 255) => {
+	const max = sequence.reduce ((prev, cur) => Math.max (prev, cur), 0) + 1
+	const squeeze =
+		Math.floor (Math.log2 (sequence.length / max))
+	const canvas = document.createElement ('canvas')
+	canvas.height = max / zoomOut
+	canvas.width = sequence.length / 2 ** squeeze / zoomOut
+	document.body.appendChild (canvas)
+	const ctx = canvas.getContext ('2d')
+	ctx.fillStyle = '#fff'
+	ctx.fillRect (0, 0, sequence.length / 2 ** squeeze / zoomOut, max / zoomOut)
+	ctx.fillStyle = 'rgb(0 0 0 /' + opacity + ')'
+	sequence.forEach (
+		(val, idx) => ctx.fillRect (idx / 2 ** squeeze / zoomOut, val / zoomOut, 1, 1)
+	)
+}
+
+window.drawfire = (depth, zoomOut, opacity) =>
+	scatterplot (calculate (depth), zoomOut, opacity)
