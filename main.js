11079e7a96baf7d615f9614cfe289d1550f308f3
sofia <12581975+opensofias@users.noreply.github.com>
2023-09-10 23:39:42 +0200

    other sequence generator functions

--- main.js
+++ main.js
@@ -22,6 +22,98 @@ const a229037 = depth => {
 	return result
 }
 
+const a229037_antiprogression = depth => {
+	const result = new Int32Array (depth)
+	let progress = 0
+	let maxVal = 1
+	while (progress < depth) {
+		const avoid = new Int8Array ((maxVal + 1) * 2)
+		let steps = 1
+		while (steps * 2 < progress + 1) {
+			avoid [
+				2 * result [progress - steps * 2] -
+				result [progress - steps]
+			] = 1
+			steps ++
+		}
+		let candidate = 0
+		while (avoid [candidate]) candidate ++
+		maxVal = Math.max (maxVal, candidate)
+		result [progress] = candidate
+		progress ++
+	}
+	return result
+}
+
+// Lexicographically earliest sequence of positive integers without triples in weakly increasing arithmetic progression. 
+const A309890 = depth => {
+	const result = new Int32Array (depth)
+	let progress = 0
+	let maxVal = 1
+	while (progress < depth) {
+		const avoid = new Int8Array ((maxVal + 1) * 2)
+		let steps = 1
+		while (steps * 2 < progress + 1) {
+			const p1 = result [progress - steps]
+			const p2 = result [progress - steps * 2]
+			p1 >= p2 && (avoid [2 * p1 - p2] = 1)
+			steps ++
+		}
+		let candidate = 0
+		while (avoid [candidate]) candidate ++
+		maxVal = Math.max (maxVal, candidate)
+		result [progress] = candidate
+		progress ++
+	}
+	return result
+}
+
+// Lexicographically earliest sequence of positive integers without triples in weakly decreasing arithmetic progression. 
+const A309890_decreasing = depth => {
+	const result = new Int32Array (depth)
+	let progress = 0
+	let maxVal = 1
+	while (progress < depth) {
+		const avoid = new Int8Array ((maxVal + 1) * 2)
+		let steps = 1
+		while (steps * 2 < progress + 1) {
+			const p1 = result [progress - steps]
+			const p2 = result [progress - steps * 2]
+			p1 <= p2 && (avoid [2 * p1 - p2] = 1)
+			steps ++
+		}
+		let candidate = 0
+		while (avoid [candidate]) candidate ++
+		maxVal = Math.max (maxVal, candidate)
+		result [progress] = candidate
+		progress ++
+	}
+	return result
+}
+
+// Lexicographically earliest sequence of positive integers without triples in constant arithmetic progression. 
+const A100480 = depth => {
+	const result = new Int32Array (depth)
+	let progress = 0
+	let maxVal = 1
+	while (progress < depth) {
+		const avoid = new Int8Array (maxVal)
+		let steps = 1
+		while (steps * 2 < progress + 1) {
+			const p1 = result [progress - steps]
+			const p2 = result [progress - steps * 2]
+			p1 == p2 && (avoid [p1] = 1)
+			steps ++
+		}
+		let candidate = 0
+		while (avoid [candidate]) candidate ++
+		maxVal = Math.max (maxVal, candidate)
+		result [progress] = candidate
+		progress ++
+	}
+	return result
+}
+
 const scatterplot = (sequence, zoomOut = 1, opacity = 255) => {
 	const max = sequence.reduce ((prev, cur) => Math.max (prev, cur), 0) + 1
 	const squeeze =
@@ -40,4 +132,4 @@ const scatterplot = (sequence, zoomOut = 1, opacity = 255) => {
 }
 
 window.drawfire = (depth, zoomOut, opacity) =>
-	scatterplot (a229037 (depth), zoomOut, opacity)
+	scatterplot (A100480 (depth), zoomOut, opacity)
