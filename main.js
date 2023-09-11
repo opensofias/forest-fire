060747609ecab777b7339aabdf2b4d7296d86c54
sofia <12581975+opensofias@users.noreply.github.com>
2023-09-11 11:03:02 +0200

    factorize out filter functions

--- main.js
+++ main.js
@@ -1,117 +1,82 @@
-// original "forest fire" function: avoid three points in arithmetric progression
-const a229037 = depth => {
-	const result = new Int32Array (depth)
+const filterSequence = (filterFunc, depth) => {
+	const sequence = new Int32Array (depth)
 	let progress = 0
 	let maxVal = 1
 	while (progress < depth) {
-		const avoid = new Int8Array ((maxVal + 1) * 2)
-		let steps = 1
-		while (steps * 2 < progress + 1) {
-			avoid [
-				2 * result [progress - steps] -
-				result [progress - steps * 2]
-			] = 1
-			steps ++
-		}
+		const avoid = filterFunc ({maxVal, progress, sequence})
 		let candidate = 0
 		while (avoid [candidate]) candidate ++
 		maxVal = Math.max (maxVal, candidate)
-		result [progress] = candidate
+		sequence [progress] = candidate
 		progress ++
 	}
-	return result
+	return sequence
 }
 
-const a229037_antiprogression = depth => {
-	const result = new Int32Array (depth)
-	let progress = 0
-	let maxVal = 1
-	while (progress < depth) {
-		const avoid = new Int8Array ((maxVal + 1) * 2)
-		let steps = 1
-		while (steps * 2 < progress + 1) {
-			avoid [
-				2 * result [progress - steps * 2] -
-				result [progress - steps]
-			] = 1
-			steps ++
-		}
-		let candidate = 0
-		while (avoid [candidate]) candidate ++
-		maxVal = Math.max (maxVal, candidate)
-		result [progress] = candidate
-		progress ++
+// original "forest fire" function: avoid three points in arithmetric progression
+const a229037_filter = ({maxVal, progress, sequence}) => {
+	const avoid = new Int8Array ((maxVal + 1) * 2)
+	let steps = 1
+	while (steps * 2 < progress + 1) {
+		avoid [
+			2 * sequence [progress - steps] -
+			sequence [progress - steps * 2]
+		] = 1
+		steps ++
 	}
-	return result
+	return avoid
+}
+
+const a229037_antiprogression_filter = ({maxVal, progress, sequence}) => {
+	const avoid = new Int8Array ((maxVal + 1) * 2)
+	let steps = 1
+	while (steps * 2 < progress + 1) {
+		avoid [
+			2 * sequence [progress - steps * 2] -
+			sequence [progress - steps]
+		] = 1
+		steps ++
+	}
+	return avoid
 }
 
 // Lexicographically earliest sequence of positive integers without triples in weakly increasing arithmetic progression. 
-const A309890 = depth => {
-	const result = new Int32Array (depth)
-	let progress = 0
-	let maxVal = 1
-	while (progress < depth) {
-		const avoid = new Int8Array ((maxVal + 1) * 2)
-		let steps = 1
-		while (steps * 2 < progress + 1) {
-			const p1 = result [progress - steps]
-			const p2 = result [progress - steps * 2]
-			p1 >= p2 && (avoid [2 * p1 - p2] = 1)
-			steps ++
-		}
-		let candidate = 0
-		while (avoid [candidate]) candidate ++
-		maxVal = Math.max (maxVal, candidate)
-		result [progress] = candidate
-		progress ++
+const A309890_filter = ({maxVal, progress, sequence}) => {
+	const avoid = new Int8Array ((maxVal + 1) * 2)
+	let steps = 1
+	while (steps * 2 < progress + 1) {
+		const p1 = sequence [progress - steps]
+		const p2 = sequence [progress - steps * 2]
+		p1 >= p2 && (avoid [2 * p1 - p2] = 1)
+		steps ++
 	}
-	return result
+	return avoid
 }
 
 // Lexicographically earliest sequence of positive integers without triples in weakly decreasing arithmetic progression. 
-const A309890_decreasing = depth => {
-	const result = new Int32Array (depth)
-	let progress = 0
-	let maxVal = 1
-	while (progress < depth) {
-		const avoid = new Int8Array ((maxVal + 1) * 2)
-		let steps = 1
-		while (steps * 2 < progress + 1) {
-			const p1 = result [progress - steps]
-			const p2 = result [progress - steps * 2]
-			p1 <= p2 && (avoid [2 * p1 - p2] = 1)
-			steps ++
-		}
-		let candidate = 0
-		while (avoid [candidate]) candidate ++
-		maxVal = Math.max (maxVal, candidate)
-		result [progress] = candidate
-		progress ++
+const A309890_but_decreasing_filter = ({maxVal, progress, sequence}) => {
+	const avoid = new Int8Array ((maxVal + 1) * 2)
+	let steps = 1
+	while (steps * 2 < progress + 1) {
+		const p1 = sequence [progress - steps]
+		const p2 = sequence [progress - steps * 2]
+		p1 <= p2 && (avoid [2 * p1 - p2] = 1)
+		steps ++
 	}
-	return result
+	return avoid
 }
 
 // Lexicographically earliest sequence of positive integers without triples in constant arithmetic progression. 
-const A100480 = depth => {
-	const result = new Int32Array (depth)
-	let progress = 0
-	let maxVal = 1
-	while (progress < depth) {
-		const avoid = new Int8Array (maxVal)
-		let steps = 1
-		while (steps * 2 < progress + 1) {
-			const p1 = result [progress - steps]
-			const p2 = result [progress - steps * 2]
-			p1 == p2 && (avoid [p1] = 1)
-			steps ++
-		}
-		let candidate = 0
-		while (avoid [candidate]) candidate ++
-		maxVal = Math.max (maxVal, candidate)
-		result [progress] = candidate
-		progress ++
+const A100480_filter = ({maxVal, progress, sequence}) => {
+	const avoid = new Int8Array (maxVal)
+	let steps = 1
+	while (steps * 2 < progress + 1) {
+		const p1 = sequence [progress - steps]
+		const p2 = sequence [progress - steps * 2]
+		p1 == p2 && (avoid [p1] = 1)
+		steps ++
 	}
-	return result
+	return avoid
 }
 
 const scatterplot = (sequence, zoomOut = 1, opacity = 255) => {
